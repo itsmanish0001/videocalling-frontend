aaff11ac3756b6bbc4b7dc6d46c73f0e4318a926
itsmanish0001 <itsmanish0001@gmail.com>
2024-05-30 00:09:09 +0530

    backend url updated

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -10,7 +10,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://videocalling-server.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
